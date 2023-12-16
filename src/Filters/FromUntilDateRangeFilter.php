89b6d725d508a9cbd3ccf2f241fa4d4659a93d9e
Loai Dev <loaidev64@gmail.com>
2023-12-16 11:46:58 +0200

    added FromUntilDateRangeFilter

---
+++ src/Filters/FromUntilDateRangeFilter.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace LaravelOdyssey\Filament\Filters;
+
+use Exception;
+use Filament\Forms\Components\DatePicker;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Tables\Filters\Filter;
+
+class FromUntilDateRangeFilter extends Filter
+{
+    public static function make(?string $name = null): static
+    {
+        $filterClass = static::class;
+
+        $name ??= static::getDefaultName();
+
+        if (blank($name)) {
+            throw new Exception("Filter of class [$filterClass] must have a unique name, passed to the [make()] method.");
+        }
+
+        $static = app($filterClass, ['name' => $name]);
+        $static->configure();
+
+        self::form([
+            DatePicker::make('from'),
+            DatePicker::make('until'),
+        ])
+            ->query(function (Builder $query, array $data) use ($name): Builder {
+                return $query
+                    ->when(
+                        $data['from'],
+                        fn (Builder $query, $date): Builder => $query->whereDate($name, '>=', $date),
+                    )
+                    ->when(
+                        $data['until'],
+                        fn (Builder $query, $date): Builder => $query->whereDate($name, '<=', $date),
+                    );
+            });
+
+        return $static;
+    }
+}
