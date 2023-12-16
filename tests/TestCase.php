9197e007c54e1ec32b0c4b083f06dcf568e24b12
loaidev64 <loaidev64@users.noreply.github.com>
2023-12-16 08:48:10 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace LaravelOdyssey\Filament\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use LaravelOdyssey\Filament\FilamentServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
@@ -13,7 +13,7 @@ class TestCase extends Orchestra
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'LaravelOdyssey\\Filament\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
+            fn (string $modelName) => 'LaravelOdyssey\\Filament\\Database\\Factories\\'.class_basename($modelName).'Factory'
         );
     }
 
